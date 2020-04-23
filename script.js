9abce6e0d7254e74d65d0a7d2d2e9801366a9545
hernanjkd <hernanjkd@gmail.com>
2020-04-22 23:21:05 -0400

    fetch testing

---
+++ script.js
@@ -0,0 +1,31 @@
+const fetch = require(`node-fetch`)
+
+function search() {
+    fetch(`https://assets.breatheco.de/apis/fake/zips.php`)
+        .then(response => {
+            return response.json();
+        }).then((jsonResponse) => {
+            setState({ businesses: jsonResponse )
+        }
+            // if (jsonResponse.businesses) {
+            // return jsonResponse.businesses.map(business => {
+            //     console.log(business)
+            //     return {
+            //         id: business.id,
+            //         imageSrc: business.image_url,
+            //         name: business.name,
+            //         address: business.location.address1,
+            //         city: business.location.city,
+            //         state: business.location.state,
+            //         zipCode: business.location.zip_code,
+            //         category: business.categories[0].title,
+            //         rating: business.rating,
+            //         reviewCount: business.review_count
+            //     }
+            // });
+            // }
+        })
+}
+
+
+search().then(data => data)
