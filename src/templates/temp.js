c57d8a103846e705967d22bc268df3cb40a93219
hernanjkd <hernanjkd@gmail.com>
2020-04-23 23:59:56 -0400

    back to normal

--- src/templates/temp.js
+++ src/templates/temp.js
@@ -32,8 +32,18 @@ const Pricing = (props) => {
                 height="500px"
                 backgroundSize="cover"
             >
+                <Divider height="100px" />
+                <Title
+                    size="5"
+                    color={Colors.white}
+                    title={yml.banner.tagline}
+                    paragraph={yml.banner.sub_heading}
+                    main
+                    paragraphColor={Colors.white}
+                    fontSize="46px"
+                    textAlign="center"
 
-
+                />
             </Wrapper>
             {/* CREDENTIALS SECTION */}
             <Wrapper
@@ -47,7 +57,18 @@ const Pricing = (props) => {
                     <Column size="1" />
                     <Column size="11" >
                         <Row>
-
+                            <Column size="1" />
+                            <Column size="10">
+                                <Row>
+                                    <Column size="5" height="300px">
+                                        <RoundImage url={yml.intro.image} height="400px" bsize="contain" />
+                                    </Column>
+                                    <Column size="4">
+                                        <Divider height="100px" />
+                                        <H5 uppercase align="left" fontSize="20px" fontHeight="30px">{yml.intro.content}</H5>
+                                    </Column>
+                                </Row>
+                            </Column>
                         </Row>
                     </Column>
                 </Row>
@@ -79,10 +100,83 @@ const Pricing = (props) => {
                     primary
                 />
                 <Divider height="30px" />
+                <Row align="center">
+                    <Modal
+                        aria-labelledby="simple-modal-title"
+                        aria-describedby="simple-modal-description"
+                        open={open}
+                        onClose={handleClose}
+                    ><div style={modalStyle} className={classes.paper}>
+                            <Row height="20%" align="center">
+                                <Column size="12" align="center"><H4>REVIEW GUIDEBOOK</H4></Column>
+                            </Row>
+                            <Row height="70%">
+                                <Column size="12">
+                                    <Row height="30%" align="center">
+                                        <Column size="11" >
+                                            <Input
+                                                type="text" className="form-control" placeholder="First name *"
+                                                onChange={(e) => setVal({ ...formData, first_name: e.target.value })}
+                                                value={formData.firstName}
+                                            />
+                                        </Column>
+                                    </Row>
+                                    <Row height="30%" align="center">
+                                        <Column size="11">
+                                            <Input type="text" className="form-control" placeholder="Last Name *"
+                                                onChange={(e) => setVal({ ...formData, last_name: e.target.value })}
+                                                value={formData.lastName}
+                                            />
+                                        </Column>
+                                    </Row>
+                                    <Row height="30%" align="center">
+                                        <Column size="11">
+                                            <Input type="email" className="form-control" placeholder="Email *"
+                                                onChange={(e) => setVal({ ...formData, email: e.target.value })}
+                                                value={formData.email}
+                                            />
+                                        </Column>
+                                    </Row>
+                                </Column>
+                            </Row>
+                            <Row height="10%" padding="5px 0 0 0" borderTop={`1px solid ${Colors.blue}`}>
 
+                                <Column size="6" customRespSize respSize="6">
+                                    <Paragraph>{formMessage}</Paragraph>
+                                </Column>
+                                <Column size="3" customRespSize respSize="3" align="right">
+                                    {
+                                        formData.first_name &&
+                                            formData.last_name &&
+                                            formData.email ?
+                                            <Button width="100%" padding=".2rem .45rem" color={Colors.blue} textColor={Colors.white}
+                                                onClick={() => {
+                                                    reviewGuidebook(formData)
+
+                                                }}>Submit</Button> : null}
+                                </Column>
+                                <Column size="3" customRespSize respSize="3" align="right">
+                                    <Button outline width="100%" padding=".2rem .45rem" color={Colors.red} textColor={Colors.white} onClick={handleClose}>Close</Button>
+                                </Column>
+                            </Row>
+
+
+                        </div>
+                    </Modal>
+                    <Button outline position="relative" width="300px" onClick={handleOpen} color={Colors.blue}>{yml.payment_guide.button_text}</Button>
+                </Row>
                 <Divider height="100px" />
             </Wrapper>
-
+            <Wrapper
+                style="default"
+                image="no"
+                color={Colors.lightGray}
+                border="top"
+            >
+                <Divider height="20px" />
+                <WhoIsHiring source={yml.ecosystem.partners_name} lang={data.allPartnerYaml.edges} />
+                <Divider height="150px" />
+            </Wrapper>
 
         </ >
     )
