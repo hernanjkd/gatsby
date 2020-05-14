498ed3d5db9b99d24e27e283290afa8bdac4b961
hernanjkd <hernanjkd@gmail.com>
2020-05-13 20:11:55 -0400

    bootstrap graphql

--- src/components/header.js
+++ src/components/header.js
@@ -1,53 +1,49 @@
-import { Link, graphql } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle, slug, data }) => (
-  <header
-    className="d-flex justify-content-around"
-    style={{
-      // backgroundImage: `url(../images/${slug}_bg.png)`,
-      // backgroundRepeat: `repeat`,
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-    {slug && <img src={require(`../images/${slug}_logo.png`)} alt={slug} />}
-  </header>
-)
-
-export const query = graphql`
-  query Links {
-    allDataYaml {
-      edges {
-        node {
-          meta_info {
-            slug
+const Header = ({ siteTitle, slug }) => {
+  const data = useStaticQuery(graphql`
+    query Links {
+      allDataYaml {
+        edges {
+          node {
+            meta_info {
+              slug
+            }
           }
         }
       }
     }
-  }
-`
+  `)
+
+  return (
+    <header
+      className="d-flex justify-content-around pt-3"
+      style={{
+        // backgroundImage: `url(../images/${slug}_bg.png)`,
+        // backgroundRepeat: `repeat`,
+        background: `rebeccapurple`
+      }}
+    >
+      <div>
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+      {slug && <img src={require(`../images/${slug}_logo.png`)} alt={slug} />}
+    </header>
+  )
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
